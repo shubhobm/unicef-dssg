e286a5f36bf0578ffa3275aafd014d95f36d9248
ChristinaLast <christina.last@outlook.com>
2020-09-13 00:03:28 +0100

    adding lib and helper files

---
+++ unicef_dssg/config.py
@@ -0,0 +1,10 @@
+# Directory structure
+SECONDARY_DATA_SOURCES = "data/Secondary_sources/"
+PROCESSED_DATA_SOURCES = "data/Processed_data/"
+CONCATENATED_DATA_SOURCES = "data/Concatenated_data/"
+
+POP_DEN = "POP_DEN/"
+LAND_USE = "LAND_USE/"
+PRECIP = "PRECIP/"
+NO2 = "NO2/"
+AOD = "AOD/"
---
+++ unicef_dssg/lib/helper.py
@@ -0,0 +1,28 @@
+import csv
+import os
+from datetime import datetime
+from typing import Any, Dict, List
+
+import pandas as pd
+
+
+class Helper:
+    @staticmethod
+    def generate_log_csv_row(log_dict: Dict[str, Any], log_columns: List[str]):
+        log_dict["datetime"] = str(datetime.now())
+        row = [log_dict[column] for column in log_columns]
+
+        return row
+
+    @staticmethod
+    def write_to_csv(row: List[Any], file_path: str) -> None:
+        with open(file_path, "a+") as log_csv:
+            writer = csv.writer(log_csv, delimiter=",")
+            writer.writerow(row)
+
+    @staticmethod
+    def read_files_in_dir(dir_path: str) -> List[str]:
+        to_remove = [".DS_Store", "", ".keep"]
+
+        files = os.listdir(dir_path)
+        return [item for item in files if item not in to_remove]
