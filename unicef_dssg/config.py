b46d3ae83ccde7c440a26d654e7b85d2aa8362f8
ChristinaLast <christina.last@outlook.com>
2020-09-13 00:37:11 +0100

    adding year directories

--- unicef_dssg/config.py
+++ unicef_dssg/config.py
@@ -3,6 +3,9 @@ SECONDARY_DATA_SOURCES = "data/Secondary_sources/"
 PROCESSED_DATA_SOURCES = "data/Processed_data/"
 CONCATENATED_DATA_SOURCES = "data/Concatenated_data/"
 
+YEAR_2019 = "2019/"
+YEAR_2020 = "2020/"
+
 POP_DEN = "POP_DEN/"
 LAND_USE = "LAND_USE/"
 PRECIP = "PRECIP/"
