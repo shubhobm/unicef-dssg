378ce3acb662dc21488587ea937668c59a0073d2
ChristinaLast <christina.last@outlook.com>
2020-09-14 11:00:18 +0100

    modifying script to take list of dfs

--- scripts/preprocess_ee_data.py
+++ scripts/preprocess_ee_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import bootstrap  # noqa
+import os
 from unicef_dssg.lib.helper import Helper
 from unicef_dssg.config import (
     PROCESSED_DATA_SOURCES,
@@ -17,32 +18,27 @@ from tqdm import tqdm
 class PreprocessEEData:
     def execute(
         self,
-        variable_name,
         df_list,
     ):
-        df = self.concat_year_df(df_list)
-        df = self.creating_datetime_column(df)
-        df.to_pickle(f"{CONCATENATED_DATA_SOURCES}LAND_USE.pkl")
+        df = self.creating_datetime_column(self._concat_year_df(df_list))
+        df.to_pickle(f"{CONCATENATED_DATA_SOURCES}NO2.pkl")
 
     def creating_datetime_column(self, df):
         df["date"] = [x[0:10] for x in df["date_range"]]
         df["date"] = pd.to_datetime(df["date"])
         return df
 
-    def concat_year_df(self, df_list):
+    def _concat_year_df(self, df_list):
         return pd.concat(df_list).reset_index(drop=True)
 
 
 if __name__ == "__main__":
-    variable_pkl = Helper.read_files_in_dir(PROCESSED_DATA_SOURCES + LAND_USE)
+    variable_pkl = Helper.read_files_in_dir(PROCESSED_DATA_SOURCES + NO2)
     variable_set = set(variable_pkl)
+    list_of_dfs = []
     Parallel(n_jobs=-1, prefer="threads", verbose=5)(
         delayed(PreprocessEEData().execute)(
-            variable_name[:-4],
-            list(
-                pd.read_pickle(f"{PROCESSED_DATA_SOURCES}{LAND_USE}{variable_name}"),
-                pd.read_pickle(f"{PROCESSED_DATA_SOURCES}{LAND_USE}{variable_name}"),
-            ),
+            list_of_dfs.append(pd.read_pickle(f"{PROCESSED_DATA_SOURCES}{NO2}{variable_name}"))
         )
         for variable_name in tqdm(variable_set)
     )
