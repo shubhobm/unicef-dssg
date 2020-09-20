9bad75a3362e5abbe7bee36e9421601df58e6ae8
ChristinaLast <christina.last@outlook.com>
2020-09-20 21:23:59 +0100

    adding details to config

--- unicef_dssg/config.py
+++ unicef_dssg/config.py
@@ -3,6 +3,7 @@ SECONDARY_DATA_SOURCES = "data/Secondary_sources/"
 PROCESSED_DATA_SOURCES = "data/Processed_data/"
 CONCATENATED_DATA_SOURCES = "data/Concatenated_data/"
 
+SATELLITE_DATA = "satellite_data/"
 YEAR_2019 = "2019/"
 YEAR_2020 = "2020/"
 
